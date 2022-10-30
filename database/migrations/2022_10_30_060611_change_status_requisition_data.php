bca3712f484c9898b180cc3de26c26f2b1ac4046
jahidqq <jahid@quadque.digital>
2022-10-30 12:09:40 +0600

    change status data type on requisition

---
+++ database/migrations/2022_10_30_060611_change_status_requisition_data.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeStatusRequisitionData extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('requisitions', function (Blueprint $table) {
+            //
+            $table->smallInteger('status')->change()->default('0');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('requisitions', function (Blueprint $table) {
+            //
+        });
+    }
+}
