a3ec9eb8cf9642dc3ad5ec5880c2a5e48cfbdccb
TanjibQuad <tanjib@quadque.tech>
2024-01-17 12:00:49 +0600

    bug fix

--- app/Http/Controllers/CompanySalesTeamController.php
+++ app/Http/Controllers/CompanySalesTeamController.php
@@ -33,7 +33,7 @@ class CompanySalesTeamController extends Controller
     {
         if ($request->bearerToken()) {
             $userApi = env('USER_SERVICE_API', '');
-            $flag = Http::timeout(10)->withToken($request->bearerToken())->post($userApi . '/check-if-token-exists');
+            $flag = Http::timeout(-1)->withToken($request->bearerToken())->post($userApi . '/check-if-token-exists');
             $flag_receive = $flag['data'];
             if ($flag_receive == 1) {
                 $company_employee = CompanySalesEmployee::where('active', 1)->get();
