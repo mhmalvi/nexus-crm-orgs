d435179702ec5043ea0db94a317d52d6583c639d
TanjibQuad <tanjib@quadque.tech>
2024-03-20 09:24:32 +0600

    subscription expiry cron

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -924,6 +924,7 @@ class CompanyController extends Controller
         $isExists = Company::select('end_date')->where('end_date','!=','')->exists();
         if ($isExists) {
             $results = Company::select('end_date')->get();
+            dd($results);
             if ($results) {
                 foreach ($results as $result) {
                     print_r(json_decode($result['end_date']));
